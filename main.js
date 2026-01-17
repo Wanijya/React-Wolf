576f1578a6c73b803427b29e67f5007e2d8fd473
Wanijya <wanijyabisen@gmail.com>
2026-01-17 12:27:52 +0530

    Refactor camera and renderer setup; improve light and cube initialization

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 100 );
+scene.add(camera);
+camera.position.z = 5;
+console.log(camera.position);
+
+/* MESH ==> (Shap & Material) */
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+})
+
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+console.log(cube.position);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(light);
+light.position.y = 4;
+light.position.z = 4;
+console.log(light.position);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize( innerWidth, innerHeight );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
+document.body.appendChild(renderer.domElement);
 
 function animate() {
-    
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+  cube.rotation.x += 0.01;
   renderer.render( scene, camera );
-
-  requestAnimationFrame( animate );
-
 }
-
-animate();
+renderer.setAnimationLoop(animate);
